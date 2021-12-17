af74a82dea74a55cc64da4674614f577f0b766b2
nirzaf <mfmfazrin1986@gmail.com>
2021-12-17 13:46:42 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace cancelapp
+{
+    class Program
+    {
+        static readonly ManualResetEvent waitEvent = new ManualResetEvent(false);
+
+        static void Loop(CancellationToken ct)
+        {
+            try
+            {
+                while (!ct.IsCancellationRequested)
+                {
+                    Console.WriteLine($"Thread: {Thread.CurrentThread.ManagedThreadId} tick");
+                    Thread.Sleep(300);
+                }
+            }
+            finally
+            {
+                waitEvent.Set();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+
+            var cts = new CancellationTokenSource(TimeSpan.FromSeconds(2));
+            Task.Run(() => Loop(cts.Token));
+            waitEvent.WaitOne();
+        }
+    }
+}
